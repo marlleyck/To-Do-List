dedc3dc43047a836164b26c28331bef8179c3967
Marlleyck Nathan <marlleyck@hotmail.com>
2022-06-29 21:49:08 -0300

    add functions (createItem / render)

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
+const banco = [
+    {tarefa: 'ler', status: ''},
+    {tarefa: 'estudar', status: 'checked'}
+]
 
+const getBanco = () => JSON.parse(localStorage.getItem('db_tarefa')) ?? []
+const setBanco = (db_tarefa) => localStorage.setItem('db_tarefa', JSON.stringify(db_tarefa))
+
+const createItem = (tarefa, status, index) => {
+    const div = document.createElement('div')
+    div.classList.add('tarefa')
+
+    div.innerHTML = `
+                <input type="checkbox" ${status} data-index="${index}">
+                <div>${tarefa}</div>
+                <input type="button" value="X">
+            `
+    document.querySelector('.todoList').appendChild(div)
+}
+
+const addItem = () => {
+    
+}
+
+
+const render = () => {
+    banco.forEach((item, index) => {
+        createItem(item.tarefa, item.status, index)
+    })
+}
+
+
+
+render()
 
 // Funções
 function openModal() {
@@ -6,10 +39,11 @@ function openModal() {
     newItem.classList.add('active')
 }
 
-function closeModal() {
+function closeModal(event) {
     let newItem = document.querySelector('.modal-new-item')
     newItem.classList.remove('active')
 }
+
 
 
 
